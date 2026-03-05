81dedb009da4f5cc107d0dd06a41f466b5235af1
RIPON <cricketbuzzbangla1@gmail.com>
2026-03-05 16:36:47 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,27 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === "development" ? componentTagger() : null
+  ].filter(Boolean),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+
   server: {
-    host: "::",
+    host: true,
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+
+  build: {
+    outDir: "dist",
   },
 }));
